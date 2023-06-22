ddb54b7ce16a80896b93c38619d854fe58fa7111
JThungu <josephkangethe848@gmail.com>
2023-06-21 18:24:47 -0700

    four

---
+++ np.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *_nop- nothing
+  *@head: head
+  *@counter: linenumber
+  *Return: no return
+  */
+void _nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
